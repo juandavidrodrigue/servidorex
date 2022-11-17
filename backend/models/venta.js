50b0533eccf80ca1965940e687063e0428e01de0
juandavidrodrigue <juandavid.janleo02@gmail.com>
2022-11-17 16:04:35 -0500

    5rd cambio

--- backend/models/venta.js
+++ backend/models/venta.js
@@ -1,22 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const ventaShema = mongoose.Schema(
-{
-cliente: {
-type: mongoose.Schema.Types.ObjectId,
-ref: "Usuario"
-},
-articulos:{
-type: Array,
-required: true
-},
-confirmado:{
-type: Boolean,
-default: false,
-},
-},
-{
-timestamps: true,
-}
+  {
+    cliente: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Usuario",
+    },
+    articulos: {
+      type: Array,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    confirmado: {
+      type: Boolean,
+      default: false,
+    },
+    estado: {
+      type: String,
+      default: "vigente",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
-const Venta = mongoose.model('Venta', ventaShema);
+const Venta = mongoose.model("Venta", ventaShema);
 export default Venta;
